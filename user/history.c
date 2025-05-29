eea33f9f916aae9193e08267d38ed0f95094f4a5
SeveredAsif <aritra.khan91@gmail.com>
2025-05-29 21:23:19 +0600

    custom history done

--- user/history.c
+++ user/history.c
@@ -3,9 +3,18 @@
 #include "user/user.h"
 #include "kernel/stats.h"
 
-int main(){
+int main(int argc, char *argv[]){
     struct syscall_stat s;
-    history(5,&s);
+    if (argc != 2) {
+        history(-1,&s);
+        return 0;
+    }
+    int num = atoi(argv[1]);
+    int success = history(num,&s);
+    if(success<0){
+        printf("history: syscall failed\n");
+        exit(1);
+    }
     printf("Syscall: %s, Count: %d\n", s.syscall_name, s.count);
     return 0;
 }
